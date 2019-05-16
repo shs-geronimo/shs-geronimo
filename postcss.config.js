72858abdd3b9861047e43b0628da9539c63c627c
Arne de Laat <arne@delaat.net>
2019-05-16 23:10:29 +0200

    Remove npm dependecies and related code
    Reorder CSS

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = (context) => ({
-    plugins: {
-        'postcss-each': {},
-        'stylelint': context.env === 'test' ? false : {},
-        'postcss-cssnext': context.env === 'test' ? false : {}
-    }
-})
